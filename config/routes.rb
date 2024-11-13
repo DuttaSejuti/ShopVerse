f4cf127cf776247d8571e22fbf94eb204d2bc25f
SejutiD <duttasejuti24@gmail.com>
2024-11-13 20:40:27 +0100

    added admin model with devise
    - rails generate devise Admin
    - added routes for the admins, migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  # authenticated admin users are redirected to the dashboard as home/root page
+  authenticated :admin_user do
+    root to: 'admin#index', as: :admin_dashboard
+  end
+
+  get 'admins', to: 'admin#index' # creates a path /admins that correcponds to admin#index action
 end
